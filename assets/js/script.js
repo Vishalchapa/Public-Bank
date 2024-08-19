1899d275c78bb9c3bbc1fed0a9943070422e3043
Vishal Chandra <chapavishal3@gmail.com>
2024-08-19 15:17:27 +0000

    Make three Div's in header with bank's name

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,17 @@ document.addEventListener('DOMContentLoaded', function() {
         this.style.backgroundColor = '#676767'; // Reset background color
         this.style.color = '#34r222'; // Reset text color
     });
-   // 3. Change the background color of a button on hover
+    //  3. Change the background color of a button on hover
+    const corporaterelationsBtn = document.getElementById('corporate-relations-btn');
+    corporaterelationsBtn.addEventListener('mouseover', function() {
+        this.style.backgroundColor = '#007BFF'; // Silver background on hover
+        this.style.color = '#fff'; // White text on hover
+    });
+    corporaterelationsBtn.addEventListener('mouseout', function() {
+        this.style.backgroundColor = '#676767'; // Reset background color
+        this.style.color = '#34r222'; // Reset text color
+    });
+   // 4. Change the background color of a button on hover
     const newsBtn = document.getElementById('news-btn');
     newsBtn.addEventListener('mouseover', function() {
         this.style.backgroundColor = '#007BFF'; // Silver background on hover
